d4319fcc522231f6bf17cb6d55e25d08e1144ca3
ADAllendez <alejo.diaz80.ad@gmail.com>
2025-01-24 14:32:08 -0300

    Desafios de Javascript

---
+++ Desafios/Iniciando JavaScript/Ejercicios.js
@@ -0,0 +1,20 @@
+alert('Bienvenida y bienvenido a nuestro sitio web');
+
+let nombre = "Lua";
+
+let edad = 25;
+
+var numeroDeVentas = 50;
+
+alert('Error!');
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+console.log (mensajeDeError);
+
+let nombre2 = prompt('Ingrese el nombre de usuario: ');
+
+let Edad = prompt ('Ingrese la edad');
+
+if (Edad >= 18){
+    alert('Puedes obtener tu licencia de conducir!');
+}
